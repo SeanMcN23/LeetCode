5df411f40096e69926a5e67dbc717478546f0297
Sean McNamara <98675643+SeanMcN23@users.noreply.github.com>
2026-02-02 22:08:12 -0600

    Time: 11 ms (75.45%) | Memory: 19.5 MB (37.82%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -16,25 +16,8 @@ class Solution:
             else:
                 hashset2[x]=1
         # im thinking that now what i should do is see if all of s elements are in hashset2 and all of t elements are in hashset1
-        if len(hashset1) != len(hashset2):
-            return False
-        for x in s:
-            if x in hashset2:
-                continue
-            else:
-                return False
-
-        for x in t:
-            if x in hashset1:
-                continue
-            else:
-                return False
-
-        for x in hashset1:
-            if x in hashset2:
-                print(hashset2[x])
-                if hashset1[x] != hashset2[x]:
-                    return False
-        return True
+        return hashset1 == hashset2
+            
+        
 
         
