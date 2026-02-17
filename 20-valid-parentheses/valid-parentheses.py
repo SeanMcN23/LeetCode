8e5a76cb0804182f42307a3c90f53163a81aa99d
Sean McNamara <98675643+SeanMcN23@users.noreply.github.com>
2026-02-16 22:00:32 -0600

    Time: 0 ms (100.00%) | Memory: 19.5 MB (21.16%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,23 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-         # the idea is to use stack, since we care about order and make sure everything is there
-         # use a stack and also use a hashmap defing each aspect
+         #i need a stack but i also need a map of what im looking for
+         
+         stack=[]
+         closeToOpen={")":"(","]":"[","}":"{"}
 
-         # its close to open since we always need to start with an open anyway but if a close exists then we just pop 
-         # the first element since that should be the opening bracket
-
-        stack=[]
-        closeToOpen={")": "(" , "]":"[" , "}":"{" }
-            # so now we need to make sure stack exists as well as make sure]
-            # need to check if in stack
-        for x in s:
+         for x in s:
             if x in closeToOpen:
                 if stack and stack[-1] == closeToOpen[x]:
                     stack.pop()
                 else:
                     return False
+
             else:
                 stack.append(x)
-        return True if not stack else False
+         return True if not stack else False
+
         
 
