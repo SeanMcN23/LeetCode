5cb0c7b45a3395091afceffa371ce33ead4e0390
Sean McNamara <98675643+SeanMcN23@users.noreply.github.com>
2026-02-16 21:53:14 -0600

    Time: 79 ms (26.50%) | Memory: 28.9 MB (17.47%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,16 +1,21 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        res=0
+        #sliding window approach, need 2 pointers, one a scout, one is reserver
         scout=1
+        res=0
         highest=0
 
-        while scout<len(prices):
+        while scout != len(prices):
+            while prices[scout] < prices[res]:
+                res += 1
             if prices[scout]>prices[res]:
                 highest=max(highest,prices[scout]-prices[res])
-            else:
-                res=scout
+                
             scout += 1
         return highest
+
+
+
 
             
 
