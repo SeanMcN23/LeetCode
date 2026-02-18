a201d47dde62c4486de6b895610ab3276925996a
Sean McNamara <98675643+SeanMcN23@users.noreply.github.com>
2026-02-17 21:17:13 -0600

    Time: 0 ms (100.00%) | Memory: 20.7 MB (27.78%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,15 +1,13 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         # need 2 pointers, cause we need midpoint
-        l,r= 0, len(nums)-1
-        # we need <= because we need to consider if an array only has 1 option
-        while l <= r:
-            # so this way is good, but if we have like some stupid big array, we would need to do l+((r-l)//2) - same thing but doesnt overflow now
-            # mostly needed in c++ where ints play a role
-            m= (l+r)//2
-            if(nums[m]>target):
+        l,r = 0, len(nums)-1
+
+        while l<=r:
+            m= (r+l)//2
+            if nums[m]>target:
                 r=m-1
-            elif nums[m] < target:
+            elif nums[m]<target:
                 l=m+1
             else:
                 return m
