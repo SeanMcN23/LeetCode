277d783026f7896a4536d1f016665dd463b64979
Sean McNamara <98675643+SeanMcN23@users.noreply.github.com>
2026-02-18 22:56:58 -0600

    Time: 12 ms (4.38%) | Memory: 20.6 MB (57.38%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -3,12 +3,13 @@ class Solution:
         # need 2 pointers, cause we need midpoint
         l,r = 0, len(nums)-1
 
-        while l<=r:
-            m= (r+l)//2
+        while l<= r:
+            m=(l+r) //2
+
             if nums[m]>target:
-                r=m-1
+                r -= 1
             elif nums[m]<target:
-                l=m+1
+                l += 1
             else:
                 return m
         return -1
