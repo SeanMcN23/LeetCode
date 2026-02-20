c7ffa7c6ddd945b448e927b6407fc59e8120bece
Sean McNamara <98675643+SeanMcN23@users.noreply.github.com>
2026-02-19 21:32:28 -0600

    Time: 9 ms (4.41%) | Memory: 20.6 MB (28.76%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,18 +1,20 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         # need 2 pointers, cause we need midpoint
-        l,r = 0, len(nums)-1
+        l=0
+        r=len(nums)-1
 
-        while l<= r:
-            m=(l+r) //2
+        while l<=r:
+            m= (l+r)//2
 
-            if nums[m]>target:
-                r -= 1
-            elif nums[m]<target:
+            if nums[m] < target:
                 l += 1
+            elif nums[m]>target:
+                r -= 1
             else:
                 return m
         return -1
+        
 
             
 
