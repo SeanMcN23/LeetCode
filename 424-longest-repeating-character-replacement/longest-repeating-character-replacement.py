7a9366a14b1f693330d0feb15ef99db90f198c10
Sean McNamara <98675643+SeanMcN23@users.noreply.github.com>
2026-02-23 20:10:28 -0600

    Time: 147 ms (29.75%) | Memory: 19.8 MB (13.12%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,19 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        myhash={}
+        scout,res=0,0
+        highest=0
+
+        for scout in range(len(s)):
+            myhash[s[scout]]= 1 + myhash.get(s[scout],0)
+
+            while (scout-res+1) - max(myhash.values()) > k:
+                myhash[s[res]] -= 1
+                res += 1
+
+
+            highest=max(highest, scout-res+1)
+        return highest
+        
+
+      
