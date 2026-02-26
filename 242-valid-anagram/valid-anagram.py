74b0128536069e691ff5c0553cb075a1c7d91849
Sean McNamara <98675643+SeanMcN23@users.noreply.github.com>
2026-02-25 21:14:35 -0600

    Time: 11 ms (75.82%) | Memory: 19.5 MB (64.29%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,22 +1,24 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        hashset1={}
-        hashset2={}
-        if len(s) != len(t):
-            return False
+       # lets do this one tomorrow, will do this, binary search, sliding window, then we do some med diff on hashings and arrays
+        hashmap1={}
+        hashmap2={}
 
         for x in s:
-            if x in hashset1:
-                hashset1[x] += 1
+            if x in hashmap1:
+                hashmap1[x] += 1
             else:
-                hashset1[x]=1
+                hashmap1[x] = 1
         for x in t:
-            if x in hashset2:
-                hashset2[x] += 1
+            if x in hashmap2:
+                hashmap2[x] += 1
             else:
-                hashset2[x]=1
-        # im thinking that now what i should do is see if all of s elements are in hashset2 and all of t elements are in hashset1
-        return hashset1 == hashset2
+                hashmap2[x] = 1
+        return hashmap1 == hashmap2
+
+        
+
+
             
         
 
