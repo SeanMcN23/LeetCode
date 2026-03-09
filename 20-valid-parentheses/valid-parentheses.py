e8ec65c1c3332fa358a2f6537d928a73534fbc27
Sean McNamara <98675643+SeanMcN23@users.noreply.github.com>
2026-03-09 18:45:37 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (90.26%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -5,15 +5,15 @@ class Solution:
          stack=[]
          closeToOpen={")":"(","]":"[","}":"{"}
 
-         for x in s:
-            if x in closeToOpen:
-                if stack and stack[-1] == closeToOpen[x]:
+         for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
                     stack.pop()
                 else:
                     return False
 
             else:
-                stack.append(x)
+                stack.append(c)
          return True if not stack else False
 
         
