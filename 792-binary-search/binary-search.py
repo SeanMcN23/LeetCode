78c95b18c9e4c33db5baf930243692c89af2a8dc
Sean McNamara <98675643+SeanMcN23@users.noreply.github.com>
2026-04-27 17:00:48 -0500

    Time: 0 ms (100.00%) | Memory: 20.5 MB (76.32%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -2,17 +2,19 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         # need 2 pointers, cause we need midpoint
         l,r= 0, len(nums)-1
-        # we need <= because we need to consider if an array only has 1 option
-        while l <= r:
-            m= (l+r)//2
 
-            if nums[m] < target:
-                l= m+1
-            elif nums[m] > target:
+        while l<=r:
+
+            m=(l+r)//2
+
+            if nums[m] > target:
                 r= m-1
+            elif nums[m] < target:
+                l=m+1
             else:
                 return m
         return -1
+        
         
 
             
