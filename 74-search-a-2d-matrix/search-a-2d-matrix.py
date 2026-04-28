b9777e347037d323993c9e760296ce7311f5df1d
Sean McNamara <98675643+SeanMcN23@users.noreply.github.com>
2026-04-27 19:04:07 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (79.45%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -5,31 +5,36 @@ class Solution:
         top,bot= 0, ROWS-1
 
         while top <= bot:
-            row= (top+bot)//2
 
-            if target > matrix[row][-1]:
-                top=row+1
-            elif target < matrix[row][0]:
+            row= (top+bot) //2 
+
+            if target>matrix[row][-1]:
+                top= row+1
+            elif target< matrix[row][0]:
                 bot=row-1
             else:
                 break
-        
-        if top > bot:
+
+        if not (top <= bot):
             return False
-        
+
         row= (top+bot)//2
         l,r= 0, COLS-1
 
         while l<=r:
+
             m= (r+l)//2
 
             if target > matrix[row][m]:
                 l=m+1
-            elif target < matrix[row][m]:
+            elif target< matrix[row][m]:
                 r=m-1
             else:
                 return True
-        return False
+
+        return False 
+
+        
         
            
 
