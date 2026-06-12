286ef819bd4ef3c30e8a831c592c8fa3d0153b1b
Sean McNamara <98675643+SeanMcN23@users.noreply.github.com>
2026-06-12 14:17:23 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (6.52%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -5,22 +5,47 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy= tail= ListNode()
-        while list1 and list2:
+       # this will need a dummy variable, saw it alrdy
+        dummy= ListNode(None)
+        curr=dummy
 
-            if list1.val < list2.val:
-                tail.next=list1
-                list1=list1.next
-            else:
-                tail.next=list2
+        if list1 == None and list2 == None:
+            return None
+        elif list2 == None:
+            return list1
+        elif list1 == None:
+            return list2
+
+
+        if list1.val > list2.val:
+            dummy.next=list2
+            list2 = list2.next
+        else:
+            dummy.next=list1
+            list1=list1.next
+        curr=curr.next
+       
+        while list1 != None and list2 != None:
+
+            if list1.val > list2.val:
+                curr.next=list2
                 list2=list2.next
-            tail=tail.next
-
-        if list1:
-            tail.next=list1
-        elif list2:
-            tail.next=list2
-
+             
+            else:
+                curr.next=list1
+                list1=list1.next
+            curr=curr.next
+        
+        if list1 is not None:
+            curr.next=list1
+        elif list2 is not None:
+            curr.next=list2
+        
         return dummy.next
+
+
+
+
+    
 
         
