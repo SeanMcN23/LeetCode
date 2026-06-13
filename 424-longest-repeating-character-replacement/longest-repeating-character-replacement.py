1e09890ffae66c27192e4a185d9ac16da0537f63
Sean McNamara <98675643+SeanMcN23@users.noreply.github.com>
2026-06-13 17:09:09 -0500

    Time: 143 ms (28.51%) | Memory: 19.7 MB (50.99%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,19 +1,30 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        myhash={}
+        myHash={}
         scout,res=0,0
         highest=0
 
         for scout in range(len(s)):
-            myhash[s[scout]] = 1 + myhash.get(s[scout],0)
 
-            while (scout-res+1)-max(myhash.values()) > k:
-                myhash[s[res]] -= 1
+             # need to add character to map, then check to see if k- max value is bigger then the whole
+            # window i have, which i believe is (s-r)+1
+
+            if s[scout] in myHash:
+                myHash[s[scout]] += 1
+            else:
+                myHash[s[scout]] = 1
+            while (scout-res)+1 - max(myHash.values()) > k:
+                myHash[s[res]] -= 1
                 res += 1
 
-            
-            highest=max(highest,scout-res+1)
+            highest= max (highest, (scout-res)+1)
         return highest
+            
+
+
+
+          
+
         
 
       
