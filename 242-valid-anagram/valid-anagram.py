6df5ef69f573580d7599e4c43d12c260907dd965
Sean McNamara <98675643+SeanMcN23@users.noreply.github.com>
2026-06-14 18:11:00 -0500

    Time: 11 ms (76.38%) | Memory: 19.2 MB (99.21%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,20 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
        # lets do this one tomorrow, will do this, binary search, sliding window, then we do some med diff on hashings and arrays
-        hashmap1={}
-        hashmap2={}
+        map1={}
+        map2={}
 
         for x in s:
-            if x in hashmap1:
-                hashmap1[x] += 1
-            else:
-                hashmap1[x] = 1
-        for x in t:
-            if x in hashmap2:
-                hashmap2[x] += 1
-            else:
-                hashmap2[x] = 1
-        return hashmap1 == hashmap2
+            map1[x]= map1.get(x,0) + 1
+        for y in t:
+             map2[y]= map2.get(y,0) + 1
+        
+        return map1 == map2
+        
+        
+
+
+
+        
 
         
 
