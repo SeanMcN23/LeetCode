6f714ac425d9e5d4c3461213c8f787d5c2153e1a
Sean McNamara <98675643+SeanMcN23@users.noreply.github.com>
2026-06-15 15:59:55 -0500

    Time: 23 ms (5.08%) | Memory: 19.5 MB (77.95%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,18 +1,21 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l, r = 0, len(s) - 1
+        start,end= 0, len(s)-1
 
-        while l < r:
-            while l < r and not self.alphaNum(s[l]):
-                l += 1
-            while r > l and not self.alphaNum(s[r]):
-                r -= 1
-            if s[l].lower() != s[r].lower():
+        while start < end:
+            while start < end and not self.isAlpha(s[start]):
+                start += 1
+            while end > start and not self.isAlpha(s[end]):
+                end -= 1
+            if s[start].lower() != s[end].lower():
                 return False
-            l, r = l + 1, r - 1
+            start += 1
+            end -= 1
         return True
 
-    def alphaNum(self, c):
-        return (ord('A') <= ord(c) <= ord('Z') or
-                ord('a') <= ord(c) <= ord('z') or
-                ord('0') <= ord(c) <= ord('9'))
+
+
+    
+
+    def isAlpha(self,c):
+        return (ord('0') <= ord(c) <= ord('9')) or (ord('A') <= ord(c) <= ord('Z')) or (ord('a')<= ord(c) <= ord('z'))
