fb93586d94d46a7204268ace40d88257b71b237f
Sean McNamara <98675643+SeanMcN23@users.noreply.github.com>
2026-06-15 16:10:19 -0500

    Time: 3 ms (34.29%) | Memory: 19.3 MB (27.64%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -4,26 +4,24 @@ class Solution:
         #super fundamental stack question:
 
         stack=[]
-        # stack in python gotta be a list
 
-        closedToOpen= {")":'(', ']':'[', '}':'{' }
+        # we want a dicionary for our closed to open when we encounter a closed
+
+        closedToOpen= {']':'[', ')':'(', "}":"{"}
 
         for bracket in s:
 
-            if bracket in closedToOpen and stack:
-                if stack[-1] == closedToOpen[bracket]:
+            if bracket in closedToOpen:
+                if stack and stack[-1]==closedToOpen[bracket]:
                     stack.pop()
                 else:
                     return False
 
             else:
                 stack.append(bracket)
-            
-        if not stack:
-            return True
-        else:
+        if stack:
             return False
-                   
+        return True
             
             
 
