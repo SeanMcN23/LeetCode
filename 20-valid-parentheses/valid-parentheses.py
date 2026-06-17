4c708c358f00c8323c4f7310d3fe8edf5fb9849d
Sean McNamara <98675643+SeanMcN23@users.noreply.github.com>
2026-06-16 20:27:31 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (67.21%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,28 +1,28 @@
 class Solution:
     def isValid(self, s: str) -> bool:
 
-        #super fundamental stack question:
-
         stack=[]
 
-        # we want a dicionary for our closed to open when we encounter a closed
-
-        closedToOpen= {']':'[', ')':'(', "}":"{"}
+        closedToOpen= {')':'(', "]":"[", '}':'{'}
 
         for bracket in s:
-
             if bracket in closedToOpen:
-                if stack and stack[-1]==closedToOpen[bracket]:
+                if stack and stack[-1] == closedToOpen[bracket]:
                     stack.pop()
+                    
+
                 else:
                     return False
-
+            
+            
             else:
                 stack.append(bracket)
+       
         if stack:
             return False
         return True
-            
+
+        
             
 
        
