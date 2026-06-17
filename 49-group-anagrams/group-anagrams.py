7fcccc08beaa81079ece1e81d2eb7f874f7becbf
Sean McNamara <98675643+SeanMcN23@users.noreply.github.com>
2026-06-17 08:27:53 -0500

    Time: 15 ms (43.79%) | Memory: 23.9 MB (27.48%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -2,16 +2,18 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
         #last time we did a double hashmap and basically returned that, but this time dont really have that luxury i dont believe
+
         map=defaultdict(list)
-        # so lets think and build the logic upon this, cause its practically the same, just slighly differnet
 
         for word in strs:
-            letters=[0]*26 # will represent a host of letters
-            for letter in word:
-                letters[ord(letter)-ord('a')] += 1
+            letters=[0]*26
+
+            for c in word:
+                letters[ord(c)-ord('a')] += 1
             map[tuple(letters)].append(word)
         return list(map.values())
 
+        
             
         
         
