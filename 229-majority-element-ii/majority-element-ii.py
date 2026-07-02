ceb0e577477277571c1019cdc979b2dc9e92ba9b
Sean McNamara <98675643+SeanMcN23@users.noreply.github.com>
2026-07-02 13:58:16 -0500

    Time: 14 ms (12.20%) | Memory: 23.6 MB (10.68%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,12 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         map={}
+        res=set()
 
         for num in nums:
             map[num]=map.get(num,0)+1
-
-        res=[]
-        for key in map:
-            if map[key] > len(nums)/3:
-                res.append(key)
-        return res
+            if map[num] > len(nums)/3:
+                res.add(num)
+        return list(res)
+        #res=[]
+        #for key in map:
+            #if map[key] > len(nums)/3:
+              #  res.append(key)
+       # return res
